9ff307a15eb7772741e6236e6bdbc84b2174414c
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-02-16 16:22:56 +0400

    add some

--- excersises_1_x.scm
+++ excersises_1_x.scm
@@ -51,9 +51,73 @@
 
 (a-plus-abs-b 1 -2)
 
+;; endless recursion
 (define (p) (p))
 
 (define (test x y)
   (if (= x 0)
       0
       y))
+
+;; don't try this at home
+;; if you r using applicative eval order
+;; (test 0 (p))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (sqr guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(sqr (sqrt 1000))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 5 0) 2 3)
+(new-if (= 1 1) 4 5)
+
+(define (sqrt-iter1 guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter1 (improve guess x) x )))
+
+(define (sqrt1 x)
+  (sqrt-iter1 1.0 x))
+
+(sqrt 0.00001)
+
+;; endless recursion cause of applicative eval order
+;; if is a special form, that doesn't use appl. eval order
+;; (sqrt1 9)
+
+(define (good-enough1? prev-guess curr-guess)
+  (< (abs (- prev-guess curr-guess)) 0.00000000001))
+
+(define (sqrt-iter2 prev-guess curr-guess x)
+  (if (good-enough1? prev-guess curr-guess)
+      curr-guess
+      (sqrt-iter2 curr-guess (improve curr-guess x) x)))
+
+(define (sqrt2 x)
+  (sqrt-iter2 0.9 1.0 x))
+
+(sqrt2 1.2)
+(sqrt2 9)
+(sqrt2 9000)
+(sqrt2 100500100500100500)
+(sqrt2 0.00000001)
